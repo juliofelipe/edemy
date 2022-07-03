cb603e3d024ceaf6727acdc89c3ee294a1b60b15
Julio Felipe <julio@informach.com.br>
2022-07-03 05:45:07 -0300

    feat: implementing routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 import express from "express";
 import cors from "cors";
+import { readdirSync } from "fs";
+
 const morgan = require("morgan");
 
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+readdirSync("./routes").map((r) =>
+    app.use("/api", require(`./routes/${r}`))
+);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));

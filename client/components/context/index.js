a256e5687fd72666f31781811f8396d6207bcb7a
Julio Felipe <julio@informach.com.br>
2022-07-05 06:44:49 -0300

    feat: add context api

--- client/components/context/index.js
+++ client/components/context/index.js
@@ -16,3 +16,13 @@ const rootReducer = (state, action) => {
             return state
     }
 };
+
+const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(rootReducer, initialState);
+
+    return (
+        <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+    )
+};
+
+export { Context, Provider };

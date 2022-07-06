58ed93941e75721a42d433255570287bb46e51f8
Julio Felipe <julio@informach.com.br>
2022-07-06 05:59:59 -0300

    feat: implementing logout

--- client/components/TopNav.jsx
+++ client/components/TopNav.jsx
@@ -1,16 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
 import  {
     AppstoreOutlined,
     LoginOutlined,
     UserAddOutlined,
+    LogoutOutlined,
 } from "@ant-design/icons";
+import {Context} from "../context";
+import axios from "axios";
+import {useRouter} from "next/router";
+import {toast} from "react-toastify";
 
 const { Item } = Menu;
 
 const TopNav = () => {
     const [current, setCurrent] = useState("");
+
+    const { state, dispatch } = useContext(Context);
+
+    const router = useRouter();
+
+    const logout = async () => {
+        dispatch({ type: "LOGOUT" })
+        window.localStorage.removeItem("user");
+        const { data } = await axios.get("/api/logout");
+        toast(data.message);
+        router.push("/login");
+    }
 
     useEffect(() => {
         process.browser && setCurrent(window.location.pathname)
@@ -40,6 +57,10 @@ const TopNav = () => {
                 <Link href="/register">
                     <a>Register</a>
                 </Link>
+            </Item>
+
+            <Item onClick={logout} icon={<LogoutOutlined />} className="float-right">
+                Logout
             </Item>
         </Menu>
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-import { register, login } from "../controllers/auth";
+import { register, login, logout } from "../controllers/auth";
 
 router.post("/register", register);
 router.post("/login", login);
